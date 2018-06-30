d316ce40ad969d5d4629e71cc80f428de4564351
qingyun <qingyun@yuanzhonghua.local>
2018-06-30 18:15:50 +0800

    新建了一个Test.java文件完成了主程序

--- Test.java
+++ Test.java
@@ -1,3 +1,13 @@
 public class Test {
-	
+ /**   git常用的密令
+	ls - ah 查看目录下的所有文件(包含隐藏文件)
+
+	git init 初始化git仓库
+	git add flename把文件添加到仓库工作区
+	git commit -m "说明内容" 将工作区内容提交到仓库
+	git status 查看仓库工作区状态
+	*/
+	public static void main(String[] args) {
+		
+	}
 }
